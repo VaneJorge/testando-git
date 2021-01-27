2591528355eb23ff51806662ec245c9537b40a28
VaneJorge <vanjorge.2014@gmail.com>
2021-01-27 20:04:05 -0300

    Adicionando laço de repetição

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -3,7 +3,8 @@ package aplicacao;
 public class Programa {
 
 	public static void main(String[] args) {
-		System.print.out("Hello world!");
+		for (int i = 0; i <= 10; i++) {
+			System.print.out("Hello world " + i + "!");
+		}
 	}
-
 }
